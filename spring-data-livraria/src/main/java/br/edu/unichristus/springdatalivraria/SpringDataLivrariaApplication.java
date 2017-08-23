b7820824e42e2bcf3d47002d716bb41f68b07949
thalys <thalys.melicio02@gmail.com>
2017-08-22 21:26:44 -0300

    spring-data-livraria

---
+++ spring-data-livraria/src/main/java/br/edu/unichristus/springdatalivraria/SpringDataLivrariaApplication.java
@@ -0,0 +1,12 @@
+package br.edu.unichristus.springdatalivraria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataLivrariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataLivrariaApplication.class, args);
+	}
+}
